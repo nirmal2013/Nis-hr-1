ba01607310aa50d5c155c6e18a3b7796d5950cc2
Gunavel <sgb.rated@gmail.com>
2017-01-01 00:18:26 +0530

    added production config
    integrating with heroic

---
+++ webpack.production.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'public', 'build');
+var mainPath = path.resolve(__dirname, 'src', 'main.js');
+
+var config = {
+
+  // We change to normal source mapping
+  devtool: 'source-map',
+  entry: mainPath,
+  output: {
+    path: buildPath,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: [nodeModulesPath]
+    },{
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  }
+};
+
+module.exports = config;
