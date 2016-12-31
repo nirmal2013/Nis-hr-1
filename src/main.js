3d6d69006a8ba39437b504741f7eac03efca941d
Gunavel <sgb.rated@gmail.com>
2017-01-01 02:31:58 +0530

    remove console stats

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import App from './app/app';
 
 let shouldExposeReduxTools = true;
 if(process.env.NODE_ENV === 'production') {
-  console.log(process.env.NODE_ENV);
   shouldExposeReduxTools = false;
 }
 
