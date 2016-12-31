1d0faa8312b8e2379dccad735120000bb63ca344
Gunavel <sgb.rated@gmail.com>
2017-01-01 01:23:26 +0530

    fix merge

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import { Provider } from 'react-redux';
 import AppStore from './app/redux/store';
 import App from './app/app';
 
+let shouldExposeReduxTools = true;
+if(process.env.NODE_ENV === 'production') {
+  console.log(process.env.NODE_ENV);
+  shouldExposeReduxTools = false;
+}
+
+let store = AppStore.create(shouldExposeReduxTools);
 
 render(
   <Provider store={store}>
